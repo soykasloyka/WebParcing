0fb6192f07f94108cff5fef287238d8394065239
Soyka <petr.emelianov@icloud.com>
2022-10-13 11:32:24 +0300

    Final working version

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import csv
 import asyncio
 import aiohttp
 
+books_data = []
+start_time = time.time()
+
 #This block parses pages
 async def get_page_data(session, page):
     headers = {
@@ -16,6 +19,67 @@ async def get_page_data(session, page):
 
     url = f"https://www.labirint.ru/genres/2308/?available=1&paperbooks=1&display=table&page={page}"
 
+    async with session.get(url=url, headers=headers) as response:
+        response_text = await response.text()
+
+        soup = BeautifulSoup(response_text, "lxml")
+
+        books_items = soup.find("tbody", class_="products-table__body").find_all("tr")
+
+        for bi in books_items:
+            book_data = bi.find_all("td")
+
+            try:
+                book_title = book_data[0].find("a").text.strip()
+            except:
+                book_title = "No book title"
+
+            try:
+                book_author = book_data[1].text.strip()
+            except:
+                book_author = "No author"
+
+            try:
+                book_publishing = book_data[2].find_all("a")
+                book_publishing = ":".join([bp.text for bp in book_publishing])
+            except:
+                book_publishing = "No publisher"
+
+            try:
+                book_new_price = int(book_data[3].find("div", class_="price").find("span").find("span").text.strip().replace(" ", ""))
+            except:
+                book_new_price = "No new price"
+
+            try:
+                book_old_price = int(book_data[3].find("span", class_="price-gray").text.strip().replace(" ", ""))
+            except:
+                book_old_price = "No old price"
+
+            try:
+                book_sale = round(((book_old_price - book_new_price) / book_old_price) * 100)
+            except:
+                book_sale = "No discount"
+
+            try:
+                book_status = book_data[-1].text.strip()
+            except:
+                book_status = "No status"
+
+            books_data.append(
+                {
+                    "book_title": book_title,
+                    "book_author": book_author,
+                    "book_publishing": book_publishing,
+                    "book_new_price": book_new_price,
+                    "book_old_price": book_old_price,
+                    "book_sale": book_sale,
+                    "book_status": book_status
+                }
+            )
+    #Print slows down the script a little, but adds the concept of a process step
+    #print(f"[INFO] Processed page {page}")
+
+
 #Making a task list
 async def gather_data():
     headers = {
@@ -24,6 +88,8 @@ async def gather_data():
     }
 
     url = "https://www.labirint.ru/genres/2308/?available=1&paperbooks=1&display=table"
+
+    
 
     async with aiohttp.ClientSession() as session:
         response = await session.get(url=url, headers=headers)
@@ -40,10 +106,49 @@ async def gather_data():
         await asyncio.gather(*tasks)
 
 def main():
-    pass
+    asyncio.run(gather_data())
+    cur_time = datetime.datetime.now().strftime("%d_%m_%Y_%H_%M")
+    #saving the data to .json file
+    with open(f"labirint_{cur_time}_async.json", "w") as file:
+        json.dump(books_data, file, indent=4, ensure_ascii=False)
+
+    with open(f"labirint_{cur_time}_async.csv", "w") as file:
+        writer = csv.writer(file)
+    #Data output structure
+        writer.writerow(
+            (
+                "Name of the book",
+                "Author",
+                "Publisher",
+                "Discount price",
+                "Price without discount",
+                "Discount percentage",
+                "Availability in stock"
+            )
+        )
+
+    for book in books_data:
+        #converting data to .csv file
+        with open(f"labirint_{cur_time}_async.csv", "a") as file:
+            writer = csv.writer(file)
+
+            writer.writerow(
+                (
+                    book["book_title"],
+                    book["book_author"],
+                    book["book_publishing"],
+                    book["book_new_price"],
+                    book["book_old_price"],
+                    book["book_sale"],
+                    book["book_status"]
+                )
+            )
+
+    finish_time = time.time() - start_time
+    print(f"Time spent on the script: {finish_time}")
 
 if __name__ == "__name__":
     main()
 
-start_time = time.time()
+
 
--- readme.txt
+++ readme.txt
@@ -1,1 +1,10 @@
-This is a program for asynchronous web page parsing. To speed up work from 40 seconds to 4 - Asyncio library is used here
+This is a script for asynchronous web page parsing. To speed up work from 40 seconds to 4 - Asyncio library is used here. After launch it saves data to .csv file.
+
+Data collection: 
+- Name of the book
+- Author
+- Publisher
+- Discount price
+- Price without discount
+- Discount percentage
+- Availability in stock
